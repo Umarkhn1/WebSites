488684e095c80afaeb56e896ea8bf26b08c06fda
Umarkhn1 <umaarkhn1@gmail.com>
2025-05-21 23:33:22 +0500

    Delete cr7fanweb directory

--- cr7fanweb/jquery-actions.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    // Кнопка для галереи
-    $('#toggle-gallery').click(function () {
-        $('#gallery').slideToggle(600);
-    });
-
-  
-});
--- cr7fanweb/script.js
+++
@@ -1,9 +0,0 @@
-
-    function playSound() {
-        let sound = document.getElementById("sound");
-        sound.currentTime = 0; 
-        sound.play().catch(error => console.log("Ошибка воспроизведения:", error));
-    }
-
-
-
